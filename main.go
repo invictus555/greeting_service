585c28f21311034c42b6afb8fd629f21dd007b3d
shengchao <shengchao@bytedance.com>
2023-07-17 21:30:28 +0800

    add IP and Port
    Change-Id: I8870e2a146a5db17c668ffb8c6e4fca86f71ec9c

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	"github.com/cloudwego/kitex/pkg/registry"
 	"github.com/cloudwego/kitex/server"
 	consulApi "github.com/hashicorp/consul/api"
 	"github.com/invictus555/auto_codes/greeting_service_v1/kitex_gen/greeting/greetingservice"
 	consul "github.com/kitex-contrib/registry-consul"
-	"log"
-	"net"
 )
 
 const (
@@ -35,7 +36,6 @@ func main() {
 		server.WithRegistryInfo(&registry.Info{ //设置注册信息
 			Weight:      1,                  // 权重
 			ServiceName: "greeting.service", // 服务名称
-			Addr:        &net.TCPAddr{IP: net.IPv4(10, 92, 94, 200), Port: 8088},
 		}),
 	)
 
