cbaea3d8a51ca94bb84d7411cfae10e17c41cc7d
shengchao <shengchao@bytedance.com>
2023-07-18 12:08:40 +0800

    add random port
    Change-Id: I9b8565006813b71a4fc817cf45b886e25eaa8d09

--- handler.go
+++ handler.go
@@ -7,14 +7,16 @@ import (
 )
 
 // GreetingServiceImpl implements the last service interface defined in the IDL.
-type GreetingServiceImpl struct{}
+type GreetingServiceImpl struct {
+	servicePort int
+}
 
 // SayHello implements the GreetingServiceImpl interface.
 func (s *GreetingServiceImpl) SayHello(ctx context.Context, req *greeting.Request) (resp *greeting.Response, err error) {
 	// TODO: Your code here...
 	err = nil
 	resp = &greeting.Response{
-		Message: req.Message + ", service comes from port = " + strconv.Itoa(servicePort),
+		Message: req.Message + ", service comes from port = " + strconv.Itoa(s.servicePort),
 	}
 	return
 }
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/invictus555/auto_codes/greeting_service_v1/kitex_gen/greeting"
 	"log"
+	"math/rand"
 	"net"
+	"time"
 
 	"github.com/cloudwego/kitex/pkg/registry"
 	"github.com/cloudwego/kitex/server"
@@ -12,7 +16,6 @@ import (
 )
 
 const (
-	servicePort = 8089
 	consulAddr  = "127.0.0.1:8500"
 	serviceName = "greeting.service"
 )
@@ -29,15 +32,17 @@ func main() {
 		panic("register consul failed")
 	}
 
+	servicePort := genRandomPort(8800, 8888)
+	fmt.Println("greeting.service port :", servicePort)
 	svr := greetingservice.NewServer(
-		new(GreetingServiceImpl),
+		NewGreetingServiceImpl(servicePort),
 		server.WithRegistry(r),
 		server.WithServiceAddr(&net.TCPAddr{
 			Port: servicePort, // 可通过修改端口模拟多实例服务
 			IP:   net.IPv4zero,
 		}),
 		server.WithRegistryInfo(&registry.Info{ //设置注册信息
-			Weight:      2,           // 权重,若测试带权类负载均衡算法,不同实例的权重应该不一样
+			Weight:      3,           // 权重,若测试带权类负载均衡算法,不同实例的权重应该不一样
 			ServiceName: serviceName, // 服务名称
 		}),
 	)
@@ -45,4 +50,21 @@ func main() {
 	if err := svr.Run(); err != nil { // 启动服务器
 		log.Println(err.Error())
 	}
+}
+
+func NewGreetingServiceImpl(port int) greeting.GreetingService {
+	impl := new(GreetingServiceImpl)
+	impl.servicePort = port
+	return impl
+}
+
+// 获取随机端口,方便同一测试机多实例部署
+func genRandomPort(min, max int) int {
+	if max < min {
+		min, max = max, min
+	}
+
+	rand.Seed(time.Now().Unix())
+	port := int(rand.Int31n(int32(max-min))) + min
+	return port
 }
